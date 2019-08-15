8bb70b2993a9925f8c239ff63f0b2a17d15f1128
Stan Slupecki <sslup@hermetikos.com>
2019-08-15 15:36:26 -0400

    Adds/updates read operations

--- people.py
+++ people.py
@@ -3,133 +3,59 @@ This is the people module and supports all the ReST actions for the
 PEOPLE collection
 """
 
-# System modules
-from datetime import datetime
+# import flask code
+from flask import (
+    make_response,
+    abort,
+)
 
-# 3rd party modules
-from flask import make_response, abort
-
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
-
-# Data to serve with our API
-PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp(),
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp(),
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp(),
-    },
-}
-
-
+# import app config code
+from config import db
+from models import (
+    Person,
+    PersonSchema,
+)
 
 def read_all():
     """
     This function responds to a request for /api/people
     with the complete lists of people
+
     :return:        json string of list of people
     """
     # Create the list of people from our data
-    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    people = Person.query \
+        .order_by(Person.lname) \
+        .all()
 
+    # Serialize the data for the response
+    person_schema = PersonSchema(many=True)
+    # creates an iterable compatible schema for the data
+    # many sets whether it's for iterables
+    return person_schema.dump(people).data
+    # use the schema object to dump people into a list
+    # connexion can automatically convert this list to JSON
 
-def read_one(lname):
+def read_one(person_id):
     """
-    This function responds to a request for /api/people/{lname}
+    This function responds to a request for /api/people/{person_id}
     with one matching person from people
-    :param lname:   last name of person to find
-    :return:        person matching last name
-    """
-    # Does the person exist in people?
-    if lname in PEOPLE:
-        person = PEOPLE.get(lname)
 
-    # otherwise, nope, not found
+    :param person_id:   ID of person to find
+    :return:            person matching ID
+    """
+    # Get the person requested
+    person = Person.query \
+        .filter(Person.person_id == person_id) \
+        .one_or_none()
+
+    # Did we find a person?
+    if person is not None:
+
+        # Serialize the data for the response
+        person_schema = PersonSchema()
+        return person_schema.dump(person).data
+
+    # Otherwise, nope, didn't find that person
     else:
-        abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
-        )
-
-    return person
-
-
-def create(person):
-    """
-    This function creates a new person in the people structure
-    based on the passed in person data
-    :param person:  person to create in people structure
-    :return:        201 on success, 406 on person exists
-    """
-    lname = person.get("lname", None)
-    fname = person.get("fname", None)
-
-    # Does the person exist already?
-    if lname not in PEOPLE and lname is not None:
-        PEOPLE[lname] = {
-            "lname": lname,
-            "fname": fname,
-            "timestamp": get_timestamp(),
-        }
-        return make_response(
-            "{lname} successfully created".format(lname=lname), 201
-        )
-
-    # Otherwise, they exist, that's an error
-    else:
-        abort(
-            406,
-            "Peron with last name {lname} already exists".format(lname=lname),
-        )
-
-
-def update(lname, person):
-    """
-    This function updates an existing person in the people structure
-    :param lname:   last name of person to update in the people structure
-    :param person:  person to update
-    :return:        updated person structure
-    """
-    # Does the person exist in people?
-    if lname in PEOPLE:
-        PEOPLE[lname]["fname"] = person.get("fname")
-        PEOPLE[lname]["timestamp"] = get_timestamp()
-
-        return PEOPLE[lname]
-
-    # otherwise, nope, that's an error
-    else:
-        abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
-        )
-
-
-def delete(lname):
-    """
-    This function deletes a person from the people structure
-    :param lname:   last name of person to delete
-    :return:        200 on successful delete, 404 if not found
-    """
-    # Does the person to delete exist?
-    if lname in PEOPLE:
-        del PEOPLE[lname]
-        return make_response(
-            "{lname} successfully deleted".format(lname=lname), 200
-        )
-
-    # Otherwise, nope, person to delete not found
-    else:
-        abort(
-            404, "Person with last name {lname} not found".format(lname=lname)
-        )
+        abort(404, 'Person not found for Id: {person_id}'.format(person_id=person_id))
